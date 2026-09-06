9a7546abb33cee341642a4c289a70bcf9a816b07
AdamAbbas23 <9aabbas@gmail.com>
2026-09-06 22:34:23 +0100

    changed node version

--- ecs-assignment/src/components/FullAppLayout/index.tsx
+++ ecs-assignment/src/components/FullAppLayout/index.tsx
@@ -122,7 +122,8 @@ const AppLayout: FC<PropsWithChildren<AppLayoutProps>> = ({
           e.preventDefault();
           e.stopPropagation();
           setContentType(undefined);
-          navigate(e.detail.href);
+          void navigate(e.detail.href);
+          // navigate(e.detail.href); 
         }
       },
       [navigate],
